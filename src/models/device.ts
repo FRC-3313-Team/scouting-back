3ed80e54475482355c983e4e68450092701bacc7
LouieK22 <LouieK22@users.noreply.github.com>
2019-02-15 14:27:44 -0600

    Modify toolchain configs

--- src/models/device.ts
+++ src/models/device.ts
@@ -1,4 +1,4 @@
-import { Document, Schema, Model, model } from "mongoose";
+import { Document, Schema, Model } from "mongoose";
 
 import { connections } from "../util/db";
 
@@ -9,13 +9,16 @@ export interface IDevice {
 	token?: string;
 }
 
-export interface IDeviceModel extends IDevice, Document { }
+export interface IDeviceModel extends IDevice, Document {}
 
 export const DeviceSchema: Schema = new Schema({
 	active: Boolean,
 	name: String,
 	activationCode: String,
-	token: String,
+	token: String
 });
 
-export const Device: Model<IDeviceModel> = connections.main.model<IDeviceModel>("Device", DeviceSchema);
+export const Device: Model<IDeviceModel> = connections.main.model<IDeviceModel>(
+	"Device",
+	DeviceSchema
+);
